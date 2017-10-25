4cd428bde21c07e8f351975414cf575a8b169b4a
Jonathan ES Lin <ernsheong@gmail.com>
2017-10-25 16:40:03 +0800

    Undo code injection

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const codeDiv = document.getElementById("code")
 const textDiv = document.getElementById("text")
 
-textDiv.innerText = codeDiv.innerHTML.toString()
+// textDiv.innerText = codeDiv.innerHTML.toString()
 
